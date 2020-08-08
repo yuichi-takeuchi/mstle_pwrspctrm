b516be4e23a71791c06e838631f21198ebf62ec3
yuichi-takeuchi <yuichi-takeuchi@umin.net>
2020-08-08 18:05:58 +0200

    update main_LTR1_80.mlx

--- code/lib/cut_preInductionTime_Takeuchi3.m
+++ code/lib/cut_preInductionTime_Takeuchi3.m
@@ -1,4 +1,4 @@
-function [flag] = msr_szrdrtn_Takeuchi3(RecInfo, DataStruct)
+function [flag] = cut_preInductionTime_Takeuchi3(RecInfo, DataStruct)
 % Copyright (C) 2018–2020 Yuichi Takeuchi
 
 %% Set params for main processes
@@ -8,12 +8,9 @@ cParams.nChannel = 30; % total number of channel of each rat
 cParams.ChLabel = ["MEC" "rHPC" "lHPC" "Ctx"];
 cParams.ChOrder = {1:6 7:15 16:24 25:30}; % {1:6 7:15 16:24 25:30}
 cParams.BaselineDrtn = uint64(floor(30*cParams.sr)); % 30 s baseline before stimulation
-cParams.TestDrtn = uint64(floor(180*cParams.sr)); % 180 s test period after stimulation
+cParams.TestDrtn = uint64(floor(0*cParams.sr)); % 0 s test period after stimulation
 
 %% Seizure detection: Band-pass filt-Rectification-movmean filtfilt signal pre-processing, RMS thresholding
-[ flag ] = eplpsyf_detectEvokedSeizureBPFltMvmnRMSThrshld2( RecInfo, DataStruct, cParams, true, true);
-
-%% Rutput of filtered and raw traces
-[ flag ] = figsf_checkTimedTracesOfGroups2( RecInfo, DataStruct, cParams);
+[ flag ] = eplpsyf_cutPreInductionTime1(DataStruct, cParams);
 
 end
---
+++ code/lib/sort_DatFiles.m
@@ -0,0 +1,36 @@
+function [flag] = sort_DatFiles(prefix, date, expNo, sessionNo, LTR, ratNo, orgTb)
+% Copyright (c) 2020 Yuichi Takeuchi
+
+flag = 0;
+
+% move to data folder
+currentFolder = pwd;
+cd('tmp/')
+
+idx = orgTb.LTR == LTR(ratNo) & orgTb.Date == date & orgTb.expNo1 == expNo & orgTb.expNo2 == sessionNo;
+Tb = orgTb(idx, :);
+
+datnamebase = [prefix '_' num2str(date) '_exp' num2str(expNo) '_' num2str(sessionNo)];
+FldrInfo = dir([datnamebase '_LFP500_' num2str(ratNo) '_trial*.dat']);
+
+% move files to results folder
+% control
+for i = 1:size(FldrInfo,1)
+    if Tb.MSEstm(i)
+        if Tb.Thresholded(i)
+            copyfile(FldrInfo(i).name, [FldrInfo(i).name(1:end-4) '_sccss.dat']);
+        else
+            copyfile(FldrInfo(i).name, [FldrInfo(i).name(1:end-4) '_nonsccss.dat']);
+        end
+    else
+        copyfile(FldrInfo(i).name, [FldrInfo(i).name(1:end-4) '_ctrl.dat']);
+    end
+end
+
+% come back to current folder
+cd(currentFolder)
+
+flag = 1;
+
+end
+
